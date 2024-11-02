5b07e601fe8578d456ca6beb974f745c6f11dc86
Isaac Kennington <81638887+isaackengh@users.noreply.github.com>
2024-11-02 19:11:49 +0000

    See previous commit

--- Budget Page/budget-script.js
+++ Budget Page/budget-script.js
@@ -28,10 +28,36 @@ function toggleSlider(checkbox) {
 function incomeForm(event){
   event.preventDefault(); // Prevent the form from submitting
   income = incomeInputForm.value;
-  incomeMessageElement.innerHTML=("Yearly after-tax income of", income)
+  incomeMessageElement.innerHTML = "Yearly after-tax income of £" + income;
   incomeMessageElement.style.display='block';
+  newChart()
 
 }
 
+//Charts
+const ctx = document.getElementById('budgetPie');
+function newChart(){
+  new Chart(ctx, {
+      type: 'doughnut',
+      data: {
+          labels: [
+          'Red',
+          'Blue',
+          'Yellow'
+        ],
+        datasets: [{
+          label: 'My First Dataset',
+          data: [123, 123, 123],
+          backgroundColor: [
+            'rgb(255, 99, 132)',
+            'rgb(54, 162, 235)',
+            'rgb(255, 205, 86)'
+          ],
+          hoverOffset: 4
+        }]},
+      
+      });
+      ctx.style.display='block';
+    }
 
 
