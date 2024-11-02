7ce3ff23bc78ca16602ba5783859205a40742bde
Isaac Kennington <81638887+isaackengh@users.noreply.github.com>
2024-11-02 17:39:26 +0000

    added income input field/form

--- Budget Page/budget-script.js
+++ Budget Page/budget-script.js
@@ -1,5 +1,8 @@
 // Declare a variable to hold the toggle state
-let student = false;
+var student = false;
+var income = -1;
+const studentMessageElement = document.getElementById('studentOnlyMessage');
+const incomeInputForm=document.getElementById('incomeInputField')
 
 function toggleSlider(checkbox) {
   const slider = checkbox.nextElementSibling;
@@ -12,11 +15,19 @@ function toggleSlider(checkbox) {
   slider.style.backgroundColor = student ? "#2196F3" : "#ccc";
   handle.style.transform = student ? "translateX(26px)" : "translateX(0)";
   
+
+  //temp: change student text
+
+  //Display student text?
+  if (student) {
+    studentMessageElement.style.display = 'block'; // Show the element
+    studentMessageElement.innerHTML=income //display the student's income
+  } else {
+    studentMessageElement.style.display = 'none'; // Hide the element
+  }
 }
 
-const studentMessageElement = document.getElementById('studentOnlyMessage');
-if (student) {
-  studentMessageElement.style.display = 'block'; // Show the element
-} else {
-  studentMessageElement.style.display = 'none'; // Hide the element
+function incomeForm(){
+  income= incomeInputForm.value;
 }
+
